26fee0e0e10e5480d20ab8c6ae8450e8d4f9d627
kimu805 <kimukimuok805@gmail.com>
2024-10-16 08:09:51 +0900

    Update works_controller.rb

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -18,21 +18,6 @@ class WorksController < ApplicationController
   def show
   end
 
-  def import_from_tmdb
-    tmdb_service = TmdbService.new
-    movie_data = tmdb_service.fetch_movie(params[:tmdb_id])
-    if movie_data
-      @work = Work.new(movie_data)
-      if @work.save
-        redirect_to @work, notice: "映画情報が取り込まれました。"
-      else
-        render :new, alert: "保存に失敗しました。"
-      end
-    else
-      redirect_to works_path, alert: "映画情報の取得に失敗しました。"
-    end
-  end
-
   private
 
   def set_work
