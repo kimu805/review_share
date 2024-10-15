fa9625a9935feb510e2e14c4a9a466c02e0c15c9
kimu805 <kimukimuok805@gmail.com>
2024-10-15 18:28:34 +0900

    Update tmdb_service.rb

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -8,7 +8,33 @@ class TmdbService
   end
 
   # 人気映画を取得するメソッド
-  
+  def fetch_popular_movies
+    response = HTTP.get("#{TMDB_API_URL}/movie/popular", params: { api_key: @api_key })
+    return [] unless response.status.success?
+
+    parse_movies(response.parse["results"])
+  end
+
+  # 新着映画を取得するメソッド
+  def fetch_now_playing_movies
+    response = HTTP.get("#{TMDB_API_URL}/movie/now_playing", params: { api_key: @api_key })
+    return [] unless response.status.success?
+
+    parse_movies(response.parse["results"])
+  end
+
+  # 映画のデータをぱーすして整理する
+  def parse_movies(movies)
+    movies.map do |movie|
+      title: movie["title"],
+      genre: movie["genre_ids"].join(", ")
+      description: movie["overview"],
+      release_date: movie["release_date"],
+      author_or_director: movie["director"],
+      thumbnail_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+      api_id: movie["id"]
+    end
+  end
 
   # def fetch_movie(movie_id)
   #   response = HTTP.get("#{TMDB_API_URL}/movie/#{movie_id}", params: { api_key: @api_key})
