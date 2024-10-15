65d0d09c0072462b126bf89b62632ca4e1611cf1
kimu805 <kimukimuok805@gmail.com>
2024-10-16 07:38:48 +0900

    Update tmdb_service.rb

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -50,20 +50,4 @@ class TmdbService
     end
   end
 
-  # def fetch_movie(movie_id)
-  #   response = HTTP.get("#{TMDB_API_URL}/movie/#{movie_id}", params: { api_key: @api_key})
-
-  #   return nil unless response.status.success?
-
-  #   movie_data = response.parse
-  #   {
-  #     title: movie_data["title"],
-  #     genre: movie_data["genres"].map { |g| g["name"] }.join(", "),
-  #     description: movie_data["overview"],
-  #     release_date: movie_data["release_date"],
-  #     author_or_director: movie_data["director"],
-  #     thumbnail_url: "https://image.tmdb.org/t/p/w500#{movie_data['poster_path']}",
-  #     api_id: movie_data["id"]
-  #   }
-  # end
 end
