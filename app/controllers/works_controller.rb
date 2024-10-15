78ed1f685418bab3bc4e84dc3e9c59e32b97802f
kimu805 <kimukimuok805@gmail.com>
2024-10-16 08:17:47 +0900

    Update works_controller.rb

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,4 @@
 class WorksController < ApplicationController
-  before_action :set_work, only: [:edit, :update, :show, :destroy]
 
   def index
     tmdb_service = TmdbService.new
@@ -8,19 +7,7 @@ class WorksController < ApplicationController
     @now_playing_movies = tmdb_service.fetch_now_playing_movies
   end
 
-  def new
-    @work = Work.new
-  end
-
-  def edit
-  end
-
   def show
   end
 
-  private
-
-  def set_work
-    @work = Work.find(params[:id])
-  end
 end
