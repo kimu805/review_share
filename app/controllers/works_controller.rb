ac779517d35dff9984c17406a8afefddbdef9b11
kimu805 <kimukimuok805@gmail.com>
2024-10-16 07:39:01 +0900

    Update works_controller.rb

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,6 +4,7 @@ class WorksController < ApplicationController
   def index
     tmdb_service = TmdbService.new
 
+    @popular_movies = tmdb_service.fetch_popular_movies
     @now_playing_movies = tmdb_service.fetch_now_playing_movies
   end
 
