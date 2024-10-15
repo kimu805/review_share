10fe011712451610a318f789e459184341c52106
kimu805 <kimukimuok805@gmail.com>
2024-10-15 15:15:59 +0900

    Create a service class to get movie information

---
+++ app/services/tmdb_service.rb
@@ -0,0 +1,26 @@
+require "http"
+
+class TmdbService
+  TMDB_API_URL = "https://api.themoviedb.org/3".freeze
+
+  def initialize
+    @api_key = ENV["TMDB_API_KEY"]
+  end
+
+  def fetch_movie(movie_id)
+    response = HTTP.get("#{TMDB_API_URL}/movie/#{movie_id}", params: { api_key: @api_key})
+
+    return nil unless response.status.success?
+
+    movie_data = response.parse
+    {
+      title: movie_data["title"],
+      genre: movie_data["genres"].map { |g| g["name"] }.join(", "),
+      description: movie_data["overview"],
+      release_date: movie_data["release_date"],
+      author_or_director: movie_data["director"],
+      thumbnail_url: "https://image.tmdb.org/t/p/w500#{movie_data['poster_path']}",
+      api_id: movie_data["id"]
+    }
+  end
+end
