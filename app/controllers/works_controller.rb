c2c0cfc358839917a8e318f9cc8b9c11cb21acf0
kimu805 <kimukimuok805@gmail.com>
2024-10-16 08:43:46 +0900

    Update works_controller.rb

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,18 +2,17 @@ class WorksController < ApplicationController
   before_action :set_tmdb_service
 
   def index
-    @popular_movies = tmdb_service.fetch_popular_movies
-    @now_playing_movies = tmdb_service.fetch_now_playing_movies
+    @popular_movies = @tmdb_service.fetch_popular_movies
+    @now_playing_movies = @tmdb_service.fetch_now_playing_movies
   end
 
   def show
-    movie_id = params[:id]
-    @work = tmdb_service.show_movie(movie_id)
+    @work = @tmdb_service.fetch_movie_detail(params[:id])
   end
 
   private
 
   def set_tmdb_service
-    tmdb_service = TmdbService.new
+    @tmdb_service = TmdbService.new
   end
 end
