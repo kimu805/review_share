fbea72b3a4a3452c6b7563ef4ee8bdaba5c62278
kimu805 <kimukimuok805@gmail.com>
2024-10-22 18:14:22 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "works#index"
   devise_for :users
   resources :works, only: [:index, :show]
+  get "works/search", to: "works#search", as: "search_works"
 end
