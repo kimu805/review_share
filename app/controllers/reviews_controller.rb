00d8aff57627ed2241e4cce0c87ab5f0905ef02c
kimu805 <kimukimuok805@gmail.com>
2024-10-26 19:23:12 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,14 @@
 class ReviewsController < ApplicationController
+  before_action :set_work
   def create
     @review = Review.new(review_params)
     if @review.save
-      redirect_to work_path
+      redirect_to work_path(@work)
+  end
+
+  private
+  def set_work
+    @tmdb_service = TmdbService.new
+    @work = @tmdb_service.fetch_movie_detail(params[:id])
   end
 end
