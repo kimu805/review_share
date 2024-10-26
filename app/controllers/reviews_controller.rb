dd83a6414e199ab7d391d26ad0b874a079640066
kimu805 <kimukimuok805@gmail.com>
2024-10-26 19:24:50 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,9 +4,15 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     if @review.save
       redirect_to work_path(@work)
+    else
+      render :new, alert: "レビューの投稿に失敗しました。"
+    end
   end
 
   private
+  def review_params
+    params.require(:review).permit(:rating, :comment, :spoiler)
+  end
   def set_work
     @tmdb_service = TmdbService.new
     @work = @tmdb_service.fetch_movie_detail(params[:id])
