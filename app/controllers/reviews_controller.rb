61db44c6424a20f8bbd9e4b72692cea1efc693aa
kimu805 <kimukimuok805@gmail.com>
2024-10-27 18:11:59 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,17 @@
 class ReviewsController < ApplicationController
 
   def create
+    @api_id = params[:api_id]
     @review = Review.new(review_params)
     if @review.save
-      redirect_to work_path(params[:api_id]), notice: "レビューの保存に成功しました。"
+      redirect_to work_path(@api_id), notice: "レビューの保存に成功しました。"
     else
-      redirect_to work_path(params[:api_id]), alert: "レビューの投稿に失敗しました。"
+      redirect_to work_path(@api_id), alert: "レビューの投稿に失敗しました。"
     end
   end
 
   private
   def review_params
-    params.require(:review).permit(:rating, :comment, :spoiler).merge(user_id: current_user.id, work_id: params[:api_id])
+    params.require(:review).permit(:rating, :comment, :spoiler).merge(user_id: current_user.id, api_id: @api_id)
   end
 end
