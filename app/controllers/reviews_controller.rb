f86b9027e57a4c12c984600452908de992349a86
kimu805 <kimukimuok805@gmail.com>
2024-10-27 17:54:30 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     if @review.save
-      redirect_to work_path(@work)
+      redirect_to work_path(params[:api_id]), notice: "レビューの保存に成功しました。"
     else
       render :new, alert: "レビューの投稿に失敗しました。"
     end
